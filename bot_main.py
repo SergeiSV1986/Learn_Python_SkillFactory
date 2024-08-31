7b2e8d65f19265afa4f63fb9962b99368ec0344b
SergeiVasilev <46534584+SergeiSV1986@users.noreply.github.com>
2024-08-31 09:03:03 +0300

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -1,19 +1,32 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from recipes_handler import router
-from token_data import BOT_TOKEN
+from aiogram import Bot, Dispatcher, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart, Command
+from quiz_handler import quiz_router, start_quiz
+from adoption_info import adoption_info_router
+from animal_data import API_TOKEN
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+# Регистрация роутеров
+dp.include_router(quiz_router)
+dp.include_router(adoption_info_router)
+
+@dp.message(Command('start'))
+async def start_command(message: Message, state: FSMContext):
+    await message.answer("Привет! Давай узнаем, какое у тебя тотемное животное? Начнем викторину!")
+    await start_quiz(message,  state)
+
+router = Router()
+router.message.register(start_command, CommandStart())
+dp.include_router(router)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
 
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
 
-    # Регистрация роутера
-    dp.include_router(router)
-
-    # Запуск polling
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
