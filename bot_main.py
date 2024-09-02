540df71351b054962e2d5e3b51106bc383ac2509
SergeiVasilev <46534584+SergeiSV1986@users.noreply.github.com>
2024-09-02 19:40:13 +0300

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,15 @@
+import logging
 from aiogram import Bot, Dispatcher, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import CommandStart, Command
 from quiz_handler import quiz_router, start_quiz
-from adoption_info import adoption_info_router
 from animal_data import API_TOKEN
+
+# Инициализация логгера
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
@@ -14,12 +18,18 @@ dp = Dispatcher(storage=storage)
 
 # Регистрация роутеров
 dp.include_router(quiz_router)
-dp.include_router(adoption_info_router)
+
 
 @dp.message(Command('start'))
 async def start_command(message: Message, state: FSMContext):
     await message.answer("Привет! Давай узнаем, какое у тебя тотемное животное? Начнем викторину!")
     await start_quiz(message,  state)
+
+# Обработка ошибок
+@quiz_router.errors()
+async def error_handler(update: Message, exception):
+    logger.error(f"Update {update} caused error {exception}")
+    return True
 
 router = Router()
 router.message.register(start_command, CommandStart())
@@ -27,6 +37,3 @@ dp.include_router(router)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
-
-
-
